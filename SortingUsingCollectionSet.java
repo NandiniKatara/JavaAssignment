773e216aa12074e988d1744e9e03ab21e5b25d84
NandiniKatara <162611955+NandiniKatara@users.noreply.github.com>
2024-03-15 13:52:38 +0530

    Update

--- SortingUsingCollectionSet.java
+++ SortingUsingCollectionSet.java
@@ -3,8 +3,8 @@ import java.util.*;
 import java.util.Scanner;  
 
 public class SortingUsingCollectionSet {
-
 	public static void main(String[] args) {
+		//Task 1: Write a program to print the sorted list of integers by using one of the collection set
 		int n;  
 		Scanner sc=new Scanner(System.in);  
 		System.out.print("Enter the number of elements : ");
@@ -15,20 +15,10 @@ public class SortingUsingCollectionSet {
 			array[i]=sc.nextInt();  
 		}  
 		System.out.println("Array elements are: ");  
-		//Task 1: Write a program to print the sorted list of integers by using one of the collection set
 		Set<Integer> arr = new TreeSet<>();
-//		arr.add(5);
-//		arr.add(-8);
-//		arr.add(10);
-//		arr.add(34);
-//		arr.add(-10);
-//		arr.add(5);
-//		arr.add(100);
-//		arr.add(-100);
 		for(int i=0;i<n;i++) {
 			arr.add(array[i]);
 		}
 		System.out.println("Sorted List of Integers " + arr);
 	}
-
 }
