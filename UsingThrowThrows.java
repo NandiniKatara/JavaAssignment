27c62974fc8b8ba2b9416b9f8e116d73ff716254
NandiniKatara <162611955+NandiniKatara@users.noreply.github.com>
2024-03-15 13:51:23 +0530

    first commit

---
+++ UsingThrowThrows.java
@@ -0,0 +1,34 @@
+package com.first;
+import java.util.*;
+import java.util.Scanner;  
+class NegativeRadiusException extends Exception{
+	public String toString() {
+		return "Radius cannot be negative";
+	}
+}
+public class UsingThrowThrows {
+	public static double areaOfCircle(float radius) throws NegativeRadiusException {
+		if(radius<0) {
+			throw new NegativeRadiusException();
+		}
+		double result=Math.PI * radius *radius;
+		return result;
+	}
+
+	public static void main(String[] args) {
+		float radius;  
+		Scanner sc=new Scanner(System.in);  
+		System.out.print("Enter the radius of Circle : ");
+		radius=sc.nextInt();
+		try {
+			double area= areaOfCircle(radius);
+			System.out.println("Area of the circle of radius " + radius + " is : "+ area);
+		}
+		catch(Exception e){
+			System.out.println("Exception");
+		}
+		
+
+	}
+
+}
